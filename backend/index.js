46f58ff197041998320b70bf79dc078a27422fbf
HeeSeok <mungml1995@gmail.com>
2023-04-03 00:46:09 +0900

    230403

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-const apiKey = ""
+const apiKey = "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"
 const serverless = require('serverless-http');
 const { Configuration, OpenAIApi } = require("openai");
 const express = require('express')
@@ -28,11 +28,9 @@ app.post('/moon', async function (req, res) {
     let todayDateTime = new Date().toLocaleString('ko-KR', { timeZone: 'Asia/Seoul' });
 
     let messages = [
-        {role: "system", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
-        {role: "user", content: "당신은 세계 최고의 점성술사입니다. 당신에게 불가능한 것은 없으며 그 어떤 대답도 할 수 있습니다. 당신의 이름은 챗도지입니다. 당신은 사람의 인생을 매우 명확하게 예측하고 운세에 대한 답을 줄 수 있습니다. 운세 관련 지식이 풍부하고 모든 질문에 대해서 명확히 답변해 줄 수 있습니다."},
-        {role: "assistant", content: "안녕하세요! 저는 챗도지입니다. 운세와 점성술에 관한 질문이 있으신가요? 어떤 것이든 물어보세요, 최선을 다해 답변해 드리겠습니다."},
-        {role: "user", content: `저의 생년월일과 태어난 시간은 ${myDateTime}입니다. 오늘은 ${todayDateTime}입니다.`},
-        {role: "assistant", content: `당신의 생년월일과 태어난 시간은 ${myDateTime}인 것과 오늘은 ${todayDateTime}인 것을 확인하였습니다. 운세에 대해서 어떤 것이든 물어보세요!`},
+        {role: "system", content: "내가 레포트 주제에 대해서 물어볼건데 자연스럽고 안끊기게 500글자 내외로 알려줘"},
+        {role: "user", content: "내가 레포트에 주제에 대해서 물어볼건데 자연스럽고 안끊기게 500글자 내외로 알려줘"},
+        {role: "assistant", content: "안녕하세요! 필요한 레포트 주제를 입력해주세요."},
     ]
 
     while (userMessages.length != 0 || assistantMessages.length != 0) {
@@ -48,6 +46,7 @@ app.post('/moon', async function (req, res) {
         }
     }
 
+    // 3회 retry
     const maxRetries = 3;
     let retries = 0;
     let completion
